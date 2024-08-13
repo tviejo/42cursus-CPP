6b9b5f19bc9a2ad176b09c9ae6ba863cb1ca2794
Thomas Viejo <tviejo@made-f0Br6s1.clusters.42paris.fr>
2024-08-13 19:01:15 +0200

    remove public

--- CPP_00/ex01/phonebook.hpp
+++ CPP_00/ex01/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: tviejo <tviejo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 12:21:55 by tviejo            #+#    #+#             */
-/*   Updated: 2024/08/13 12:21:27 by tviejo           ###   ########.fr       */
+/*   Updated: 2024/08/13 18:57:26 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 #include "Contact.hpp"
 
 class PhoneBook{
-    public:
-        Contact contacts[8];
+    private:
         int nb_contacts;
+        Contact contacts[8];
+    public:
         PhoneBook();
         void add_contact();
         void search_contact();

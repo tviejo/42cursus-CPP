36bbd172c04389e11df5ece16b7fae738c20d4ef
Thomas Viejo <tviejo@made-f0Br7s3.clusters.42paris.fr>
2024-09-30 18:00:14 +0200

    better iter

--- CPP_07/ex01/iter.hpp
+++ CPP_07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: tviejo <tviejo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 19:18:24 by tviejo            #+#    #+#             */
-/*   Updated: 2024/09/30 14:54:46 by tviejo           ###   ########.fr       */
+/*   Updated: 2024/09/30 17:59:27 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,54 +15,6 @@
 
 template <typename T, typename F>
 void    iter(T* array, int length, F f)
-{
-    for (int i = 0; i < length; i++) {
-        f(array[i]);
-    }
-}
-
-template <typename T, typename F>
-void    iter(const T* array, int length, F f)
-{
-    for (int i = 0; i < length; i++) {
-        f(array[i]);
-    }
-}
-
-template <typename T, typename F>
-void    iter(const T* array, int length, const F f)
-{
-    for (int i = 0; i < length; i++) {
-        f(array[i]);
-    }
-}
-
-template <typename T, typename F>
-void    iter(T* array, int length, const F f)
-{
-    for (int i = 0; i < length; i++) {
-        f(array[i]);
-    }
-}
-
-template <typename T, typename F>
-void    iter(T* array, const int length, F f)
-{
-    for (int i = 0; i < length; i++) {
-        f(array[i]);
-    }
-}
-
-template <typename T, typename F>
-void    iter(const T* array, const int length, F f)
-{
-    for (int i = 0; i < length; i++) {
-        f(array[i]);
-    }
-}
-
-template <typename T, typename F>
-void const   iter( T* array, int length,  F f)
 {
     for (int i = 0; i < length; i++) {
         f(array[i]);
